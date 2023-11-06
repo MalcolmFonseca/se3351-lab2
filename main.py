59d2a476b5302634cfa6376185722da6279bd907
Malcolm Fonseca <113556151+MalcolmFonseca@users.noreply.github.com>
2023-11-06 14:23:24 -0500

    main file created

---
+++ main.py
@@ -0,0 +1,21 @@
+def main_menu():
+    print("""Welcome to the Retail Store Management Program!
+          1. Member Management
+          2. Item Management
+          3. Inventory Viewing
+          4. Transaction Processing
+          5. Exit
+          Enter choice: """)
+    
+    choice = input()
+    if choice.isnumeric() and 0<choice<6:
+        if choice == 1:
+            pass
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            pass
+        elif choice == 4:
+            pass
+        elif choice == 5:
+            pass
